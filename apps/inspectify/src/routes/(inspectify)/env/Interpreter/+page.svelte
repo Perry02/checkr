f6ffa38f67677dddbca4ecf21dd6b71829c4d095
Pedro <phtromano@gmail.com>
2026-05-21 20:47:58 +0200

    added boundaries to trace container window

--- apps/inspectify/src/routes/(inspectify)/env/Interpreter/+page.svelte
+++ apps/inspectify/src/routes/(inspectify)/env/Interpreter/+page.svelte
@@ -190,7 +190,7 @@
     </StandardInput>
   {/snippet}
   {#snippet outputView({ input: cachedInput, output, meta })}
-    <div class="grid min-h-0 grid-cols-[auto_1fr]">
+    <div class="grid min-h-0 grid-cols-2">
       <div class="flex min-h-0 flex-col border-t border-r bg-slate-900">
         <div class="flex items-center justify-between border-b border-slate-700 px-4 py-2">
           <div class="font-mono text-sm font-semibold tracking-wider text-slate-200 uppercase">
